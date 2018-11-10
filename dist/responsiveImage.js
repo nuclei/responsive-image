c1b349dfd247a00b5fef8f28e75b9d9043ed9beb
Lukas Oppermann <oppermann.lukas@gmail.com>
2018-11-10 08:12:55 +0100

    build version

--- dist/responsiveImage.js
+++ dist/responsiveImage.js
@@ -1,18 +1,17 @@
 (function () {
-'use strict';
+    'use strict';
 
-var ready = function (fn) {
-    if (document.readyState !== 'loading') {
-        fn();
-    }
-    else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
-};
+    var ready = function (fn) {
+        if (document.readyState !== 'loading') {
+            fn();
+        }
+        else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    };
 
-'use strict';
-let template = document.createElement('template');
-template.innerHTML = `<style>
+    let template = document.createElement('template');
+    template.innerHTML = `<style>
     :host{
       display: flex;
     }
@@ -72,175 +71,166 @@ template.innerHTML = `<style>
     <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" alt="" />
   </figure>
 `;
-class ResponsiveImage extends HTMLElement {
-    constructor() {
-        super();
-        this._src = null;
-        this._srcset = null;
-        this._placeholder = null;
-        this._active = undefined;
-        this._img = null;
-        this._figure = null;
-        this._aspectRatio = null;
-        this._observer = null;
-        this._threshold = 0;
-        this._offset = `100px`;
-        let shadowRoot = this.attachShadow({ mode: 'open' });
-        if (typeof ShadyCSS !== 'undefined') {
-            ShadyCSS.prepareTemplate(template, 'responsive-image');
-            ShadyCSS.styleElement(this);
-        }
-        shadowRoot.appendChild(document.importNode(template.content, true));
-    }
-    static get observedAttributes() {
-        return ['src', 'srcset', 'placeholder', 'active', 'threshold', 'offset', 'ratio'];
-    }
-    attributeChangedCallback(attrName, oldVal, newVal) {
-        this[attrName] = newVal;
-    }
-    connectedCallback() {
-        this._setAspectRatio();
-        this._loadPlaceholder();
-        this._createObserver();
-        this._figure = this.shadowRoot.querySelector('figure');
-        this._img = this.shadowRoot.querySelector('img');
-    }
-    _loadImage() {
-        if (this._src === null || this._active !== 'true')
-            return;
-        let _img = document.createElement('img');
-        _img.addEventListener('load', () => {
-            this._img.setAttribute('src', this._src);
-            if (this._srcset !== null) {
-                this._img.setAttribute('srcset', this._srcset);
+    class ResponsiveImage extends HTMLElement {
+        constructor() {
+            super();
+            this._src = null;
+            this._srcset = null;
+            this._placeholder = null;
+            this._active = undefined;
+            this._img = null;
+            this._figure = null;
+            this._aspectRatio = null;
+            this._observer = null;
+            this._threshold = 0;
+            this._offset = `100px`;
+            let shadowRoot = this.attachShadow({ mode: 'open' });
+            if (typeof ShadyCSS !== 'undefined') {
+                ShadyCSS.prepareTemplate(template, 'responsive-image');
+                ShadyCSS.styleElement(this);
             }
-            this._fillmode();
-        });
-        ready(() => {
-            _img.setAttribute('src', this._src);
-            if (this._srcset !== null) {
-                _img.setAttribute('srcset', this._srcset);
-            }
-            this.dispatchEvent(new CustomEvent('loaded'));
-            this._destroyObserver();
-        });
-    }
-    _loadPlaceholder() {
-        if (this._placeholder === null)
-            return;
-        this.shadowRoot.querySelector('img').setAttribute('src', this._placeholder);
-    }
-    _fillmode() {
-        if (this._img.clientWidth < this._figure.clientWidth) {
-            this._img.setAttribute('fillmode', 'width');
+            shadowRoot.appendChild(document.importNode(template.content, true));
         }
-        if (this._img.clientHeight < this._figure.clientHeight) {
-            this._img.setAttribute('fillmode', 'height');
+        static get observedAttributes() {
+            return ['src', 'srcset', 'placeholder', 'active', 'threshold', 'offset', 'ratio'];
         }
-    }
-    _createObserver() {
-        if (this._active !== undefined || typeof IntersectionObserver !== 'function')
-            return;
-        this._observer = new IntersectionObserver((changes) => {
-            changes.forEach((change) => {
-                if (change.isIntersecting) {
-                    change.target.setAttribute('active', 'true');
+        attributeChangedCallback(attrName, oldVal, newVal) {
+            this[attrName] = newVal;
+        }
+        connectedCallback() {
+            this._setAspectRatio();
+            this._loadPlaceholder();
+            this._createObserver();
+            this._figure = this.shadowRoot.querySelector('figure');
+            this._img = this.shadowRoot.querySelector('img');
+        }
+        _loadImage() {
+            if (this._src === null || this._active !== 'true')
+                return;
+            let _img = document.createElement('img');
+            _img.addEventListener('load', () => {
+                this._img.setAttribute('src', this._src);
+                if (this._srcset !== null) {
+                    this._img.setAttribute('srcset', this._srcset);
                 }
             });
-        }, {
-            rootMargin: this.offset,
-            threshold: this.threshold
-        });
-        this._observer.observe(this);
-    }
-    _destroyObserver() {
-        if (this._observer === null)
-            return;
-        this._observer.unobserve(this);
-    }
-    _setAspectRatio() {
-        if (this._aspectRatio) {
-            this.shadowRoot.querySelector('figure').style.paddingBottom = this._aspectRatio + '%';
+            ready(() => {
+                _img.setAttribute('src', this._src);
+                if (this._srcset !== null) {
+                    _img.setAttribute('srcset', this._srcset);
+                }
+                this.dispatchEvent(new CustomEvent('loaded'));
+                this._destroyObserver();
+            });
         }
-    }
-    set src(src) {
-        if (this._src === src)
-            return;
-        this._src = src;
-        if (document.readyState !== 'loading') {
+        _loadPlaceholder() {
+            if (this._placeholder === null)
+                return;
+            this.shadowRoot.querySelector('img').setAttribute('src', this._placeholder);
+        }
+        _createObserver() {
+            if (this._active !== undefined || typeof IntersectionObserver !== 'function')
+                return;
+            this._observer = new IntersectionObserver((changes) => {
+                changes.forEach((change) => {
+                    if (change.isIntersecting) {
+                        change.target.setAttribute('active', 'true');
+                    }
+                });
+            }, {
+                rootMargin: this.offset,
+                threshold: this.threshold
+            });
+            this._observer.observe(this);
+        }
+        _destroyObserver() {
+            if (this._observer === null)
+                return;
+            this._observer.unobserve(this);
+        }
+        _setAspectRatio() {
+            if (this._aspectRatio) {
+                this.shadowRoot.querySelector('figure').style.paddingBottom = this._aspectRatio + '%';
+            }
+        }
+        set src(src) {
+            if (this._src === src)
+                return;
+            this._src = src;
+            if (document.readyState !== 'loading') {
+                this._loadImage();
+            }
+        }
+        get src() {
+            return this._src;
+        }
+        set srcset(srcset) {
+            if (this._srcset === srcset)
+                return;
+            this._srcset = srcset;
+        }
+        get srcset() {
+            return this._srcset;
+        }
+        set placeholder(placeholder) {
+            if (this._placeholder === placeholder)
+                return;
+            this._placeholder = placeholder;
+        }
+        get placeholder() {
+            return this._placeholder;
+        }
+        set active(active) {
+            active = (active === 'true' || active === true).toString();
+            if (this._active === active)
+                return;
+            this._active = active;
+            this.setAttribute('active', this._active);
             this._loadImage();
         }
-    }
-    get src() {
-        return this._src;
-    }
-    set srcset(srcset) {
-        if (this._srcset === srcset)
-            return;
-        this._srcset = srcset;
-    }
-    get srcset() {
-        return this._srcset;
-    }
-    set placeholder(placeholder) {
-        if (this._placeholder === placeholder)
-            return;
-        this._placeholder = placeholder;
-    }
-    get placeholder() {
-        return this._placeholder;
-    }
-    set active(active) {
-        active = (active === 'true' || active === true).toString();
-        if (this._active === active)
-            return;
-        this._active = active;
-        this.setAttribute('active', this._active);
-        this._loadImage();
-    }
-    get active() {
-        return this._active;
-    }
-    set threshold(threshold) {
-        threshold = parseFloat(parseFloat(threshold + '').toFixed(2));
-        if (isNaN(threshold) || threshold > 1 || threshold < 0)
-            threshold = 1;
-        if (this._threshold === threshold)
-            return;
-        this._threshold = threshold;
-    }
-    get threshold() {
-        return this._threshold;
-    }
-    set offset(offset) {
-        if (this._offset === offset)
-            return;
-        this._offset = offset;
-    }
-    get offset() {
-        return this._offset;
-    }
-    set ratio(aspectRatio) {
-        if (this._aspectRatio === aspectRatio)
-            return;
-        let ratios = aspectRatio.split(':');
-        let ratio = null;
-        if (ratios.length > 1) {
-            ratio = 100 * (parseInt(ratios[1]) / parseInt(ratios[0]));
+        get active() {
+            return this._active;
         }
-        else {
-            ratio = parseInt(ratios[0]);
+        set threshold(threshold) {
+            threshold = parseFloat(parseFloat(threshold + '').toFixed(2));
+            if (isNaN(threshold) || threshold > 1 || threshold < 0)
+                threshold = 1;
+            if (this._threshold === threshold)
+                return;
+            this._threshold = threshold;
         }
-        if (!isNaN(ratio)) {
-            this._aspectRatio = ratio;
-            this._setAspectRatio();
+        get threshold() {
+            return this._threshold;
+        }
+        set offset(offset) {
+            if (this._offset === offset)
+                return;
+            this._offset = offset;
+        }
+        get offset() {
+            return this._offset;
+        }
+        set ratio(aspectRatio) {
+            if (this._aspectRatio === aspectRatio)
+                return;
+            let ratios = aspectRatio.split(':');
+            let ratio = null;
+            if (ratios.length > 1) {
+                ratio = 100 * (parseInt(ratios[1]) / parseInt(ratios[0]));
+            }
+            else {
+                ratio = parseInt(ratios[0]);
+            }
+            if (!isNaN(ratio)) {
+                this._aspectRatio = ratio;
+                this._setAspectRatio();
+            }
+        }
+        get ratio() {
+            return this._aspectRatio;
         }
     }
-    get ratio() {
-        return this._aspectRatio;
-    }
-}
-window.customElements.define('responsive-image', ResponsiveImage);
+    window.customElements.define('responsive-image', ResponsiveImage);
 
 }());
 //# sourceMappingURL=responsiveImage.js.map
